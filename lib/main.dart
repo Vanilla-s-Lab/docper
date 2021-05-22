40c9c556030a60fadacf7989d6f035d1a8466ef4
Vanilla <neko@hydev.org>
2021-05-22 18:55:18 +0800

    [-] Remove an unnecessary Divider widget.

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,6 @@ class _MyHomePageState extends State<MyHomePage> {
               DockerHeader(_drawerBus),
               Divider(),
               Text("Container list", textAlign: TextAlign.center),
-              Divider(),
               DockerContainerList(_drawerBus),
             ],
           ),
